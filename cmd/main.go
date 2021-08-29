3df32c05e01254290b02d12f49c1f1a933118bb1
spudymun <spudymun@gmail.com>
2021-08-29 21:55:27 +0300

    test

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
+	"time"
 
 	"github.com/Spudymun/LRU/pkg/lrucache"
 )
 
 func main() {
 	lru := lrucache.New(5)
-	lru.Set("1", 1)
-	lru.Set("2", 2)
-	lru.Set("3", 3)
-	lru.Set("4", 4)
-	lru.Set("5", 5)
-	lru.Set("6", 6)
-	lru.Set("1", 1)
-	lru.Set("7", 7)
-	lru.Invalidate("1")
-	lru.Set("8", 8)
-	for i := 1; i <= 9; i++ {
-		fmt.Println(lru.Get(strconv.Itoa(i)))
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(20)
+	fmt.Println("n =", n)
+	for i := 0; i <= n; i++ {
+		lru.Set(strconv.Itoa(i), rand.Int31n(20))
 	}
+	lru.Set(strconv.Itoa(5), rand.Int31n(20))
 	fmt.Println(lru)
 }
